53ff7af5b50816a2933cf51a7b3e90c159d24348
Mikołaj Szymczuk <mikolajszymczuk.dev@wp.pl>
2025-12-21 21:07:20 +0100

    first steps

--- app/main.py
+++ app/main.py
@@ -1,1 +1,25 @@
-print("hello")
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Item(BaseModel):
+    item_id: int
+    name: str
+    description: str
+
+
+app = FastAPI()
+
+@app.get("/hello/{name}")
+async def hello(name: str, age: int = 0):
+    return {
+        "msg": f"Hello {name}!",
+        "age": age
+    }
+
+@app.post("/create_item")
+async def create_item(item: Item):
+    return {
+        "item_id": item.item_id,
+        "name": item.name,
+        "description": item.description
+    }
